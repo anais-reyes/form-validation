312bd9e177c0c0ae2dee491d7670cc7b4e2eb45d
anais-reyes <maria.reyes@itexico.net>
2018-06-06 08:53:29 -0500

    reducing code

--- js/app.js
+++ js/app.js
@@ -1,5 +1,25 @@
+const input = document.querySelectorAll('.input');
+
+const patterns = {
+	name: /^[a-zA-Z\s]+$/,
+	age: /^\d{1,2}$/,
+	phone: /^\d{10}$/,
+	email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z{2,8}])?$/,
+	about: /.{20,100}/,
+};
+
+const cleanStorage = () => {
+	localStorage.clear();
+};
+
 const activateButton = () => {
-	if (localStorage.name && localStorage.age && localStorage.phone && localStorage.email && localStorage.about) {
+	if (
+		document.getElementById('name').value.trim().length > 0 &&
+		document.getElementById('age').value.trim().length > 0 &&
+		document.getElementById('phone').value.trim().length > 0 &&
+		document.getElementById('email').value.trim().length > 0 &&
+		document.getElementById('about').value.trim().length > 0
+	) {
 		document.getElementById('button').removeAttribute('disabled');
 		document.getElementById('button').className = 'active';
 	} else {
@@ -8,96 +28,38 @@ const activateButton = () => {
 	}
 };
 
-const cleanStorage = () => {
-	localStorage.clear();
-};
-
-const validateNameInput = () => {
-	let status = /^[a-zA-Z\s]+$/.test(document.getElementById('name').value);
-	if (status === false) {
-		document.getElementById('name-error').style.display = 'block';
+const validInput = event => {
+	if (patterns[event.target.id].test(event.target.value)) {
+		localStorage.setItem(event.target.id, 'true');
 	} else {
-		document.getElementById('name-error').style.display = 'none';
+		localStorage.setItem(event.target.id, 'false');
 	}
 };
 
-const validateAgeInput = () => {
-	let status = /^\d{1,2}$/.test(document.getElementById('age').value);
-	if (status === false) {
-		document.getElementById('age-error').style.display = 'block';
+const validateSingleField = element => {
+	let item = element.id;
+	if (JSON.parse(localStorage[item]) === false) {
+		document.getElementById(element.id).nextSibling.nextSibling.style.display = 'block';
 	} else {
-		document.getElementById('age-error').style.display = 'none';
+		document.getElementById(element.id).nextSibling.nextSibling.style.display = 'none';
 	}
 };
 
-const validatePhoneInput = () => {
-	let status = /^\d{10}$/.test(document.getElementById('phone').value);
-	if (status === false) {
-		document.getElementById('phone-error').style.display = 'block';
-	} else {
-		document.getElementById('phone-error').style.display = 'none';
-	}
-};
-
-validateEmailInput = () => {
-	let status = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z{2,8}])?$/.test(
-		document.getElementById('email').value
-	);
-	if (status === false) {
-		document.getElementById('email-error').style.display = 'block';
-	} else {
-		document.getElementById('email-error').style.display = 'none';
-	}
-};
-
-const validateAboutInput = () => {
-	console.log();
-	if (document.getElementById('about').value.length < 20 || document.getElementById('about').length > 100) {
-		document.getElementById('about-error').style.display = 'block';
-	} else {
-		document.getElementById('about-error').style.display = 'none';
-	}
-};
-
-const validateName = event => {
-	localStorage.setItem('name', event.target.value.trim());
-	activateButton();
-};
-
-const validateAge = event => {
-	localStorage.setItem('age', event.target.value.trim());
-	activateButton();
-};
-
-const validatePhone = event => {
-	localStorage.setItem('phone', event.target.value.trim());
-	activateButton();
-};
-
-const validateEmail = event => {
-	localStorage.setItem('email', event.target.value.trim());
-	activateButton();
-};
-
-const validateAbout = event => {
-	localStorage.setItem('about', event.target.value.trim());
-	activateButton();
-};
-
-const validateInputs = event => {
+const validateFields = event => {
 	event.preventDefault();
-	validateNameInput();
-	validateAgeInput();
-	validatePhoneInput();
-	validateEmailInput();
-	validateAboutInput();
+	input.forEach(element => {
+		validateSingleField(element);
+	});
 };
 
-document.getElementById('name').addEventListener('keyup', validateName);
-document.getElementById('age').addEventListener('keyup', validateAge);
-document.getElementById('phone').addEventListener('keyup', validatePhone);
-document.getElementById('email').addEventListener('keyup', validateEmail);
-document.getElementById('about').addEventListener('keyup', validateAbout);
-document.getElementById('button').addEventListener('click', validateInputs);
+input.forEach(element => {
+	element.addEventListener('keyup', activateButton);
+});
+
+input.forEach(element => {
+	element.addEventListener('keyup', validInput);
+});
+
+document.getElementById('button').addEventListener('click', validateFields);
 
 cleanStorage();
